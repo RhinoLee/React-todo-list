91d09b2758e797f767c74e809af6df511f48c6ca
Rhino Lee <picklerhino@gmail.com>
2024-04-10 22:17:10 +0800

    style component

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from 'styled-components';
+
+const theme = {
+  colors: {
+    primary_300: "#3333dd",
+    primary_400: "#e33e3e",
+    primary_500: "#af0505",
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
     <App />
-  </React.StrictMode>
+  </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function

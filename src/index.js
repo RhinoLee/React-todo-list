c5b89048210d576eb21d4c2c9ac9fb3f614e1e3f
Rhino Lee <picklerhino@gmail.com>
2024-04-14 18:16:47 +0800

    add prettier rule & vscode setting

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ import { ThemeProvider } from 'styled-components';
 
 const theme = {
   colors: {
-    primary_300: "#3333dd",
-    primary_400: "#e33e3e",
-    primary_500: "#af0505",
-  }
-}
+    primary_300: '#3333dd',
+    primary_400: '#e33e3e',
+    primary_500: '#af0505',
+  },
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ThemeProvider theme={theme}>
     <App />
-  </ThemeProvider>
+  </ThemeProvider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
